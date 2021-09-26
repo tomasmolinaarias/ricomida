84f05057f159f0f860bbce12ebed93214305cade
tomasmolinaarias <tomasmolinaarias12@gmail.com>
2021-09-25 21:59:36 -0300

    prelista faltan algunos detalles

--- Assets /js/pagina_1.js
+++ Assets /js/pagina_1.js
@@ -1,12 +1,29 @@
-// tooltlip
-//$(function(){
-//   var tooltipList =[].slice.call( document.querySelectorAll('[data-bs-toggle="tooltip"]')
-//    );
-//    tooltipTriggerList.map(function(tooltipTriggerEl) {
-//        return new boostrap.tooltip(tooltipTriggerEl);
-//    })
-//});
+/*cambio de color*/
+$(document).ready(function(){
+  $("#titulo_ingrediente").click(function(){
+    $(this).css("color","red");
+  });
+});
+
+$(document).ready(function(){
+  $("#titulo_preparacion").click(function(){
+    $(this).css("color","red");
+  });
+});
+
+$(document).ready(function(){
+  $(".tarjetas").click(function(){
+    $(this).toggle("hide");
+  });
+});
+
+/*tooltip*/
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+/*poopovers*/
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
 })
